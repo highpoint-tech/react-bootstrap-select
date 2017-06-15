b3b9a4d845b802613ee854c493ba6315fa5d115e
Martin Ciparelli <mciparelli@gmail.com>
2017-06-15 16:05:26 -0300

    Update experiment

--- experiment/index.js
+++ experiment/index.js
@@ -1,11 +1,36 @@
 import Select from '../src/index';
 import { createElement as t } from 'react';
+import { compose, withHandlers, withState, withProps, mapProps } from 'recompose';
 import { render } from 'react-dom';
 import 'bootstrap-select';
 
-const el =
+const enhance = compose(
+  withState('value', 'set', 2),
+  withProps(({ set, value }) => ({
+    navigate(data) {
+      global.location = `another route`;
+    },
+    reset() {
+      set(value);
+    }
+  })),
+  mapProps(({ set, value, navigate, reset, ...props }) => ({
+    ...props,
+    value,
+    bs: {
+      mobile: true
+    },
+    onChange: ev => {
+      const confirmedChange = confirm('confirm change?');
+      if (confirmedChange) return void navigate();
+      reset();
+    }
+  }))
+);
+
+const El = enhance(({ value, onChange, bs }) =>
   t('div', null,
-    t(Select, null,
+    t(Select, { value, onChange, bs },
       t('option', { value: 1 }, '1 Minute'),
       t('option', { value: 2 }, '2 Minutes')
     ),
@@ -13,6 +38,7 @@ const el =
       t('option', { value: 3 }, '3 Minute'),
       t('option', { value: 4 }, '4 Minutes')
     )
-  );
+  )
+);
 
-render(el, document.querySelector('.app-container'));
+render(t(El), document.querySelector('.app-container'));
